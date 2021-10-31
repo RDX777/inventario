e4db8d4204abb2d2831a91b743d9920388c6ad24
Anderson Silva Tome <anderson_tome@outlook.com.br>
2021-10-31 04:34:36 -0300

    correção de migration

--- database/migrations/2021_10_24_084338_create_images_table.php
+++ database/migrations/2021_10_24_084338_create_images_table.php
@@ -17,21 +17,21 @@ class CreateImagesTable extends Migration
             $table->id();
             $table->string('file_name', 255);
             $table->string('file_extension');
-            //$table->binary('data');
         });
 
         DB::statement("ALTER TABLE images ADD data LONGBLOB");
 
         DB::table('images')->insert(array(
-            'id' => 0,
             'file_name' => 'no_image',
             'file_extension' => 'jpg',
             'data' => file_get_contents(public_path() . '/no_image.jpg')
 
         ));
 
-        DB::statement("UPDATE images SET id = 0 WHERE id = 1");
-        DB::statement("ALTER TABLE images AUTO_INCREMENT = 0");
+        //DB::statement("UPDATE images SET id = 0 WHERE id = 1");
+        //DB::statement("ALTER TABLE images AUTO_INCREMENT = 0");
+        //DB::statement("ALTER TABLE images MODIFY id BIGINT(20)");
+
 
         DB::table('images')->insert(array(
             'file_name' => 'micro1',
